dcdb121c3b4baa4de6acef5e29690050e9ba834d
sdg <songdaegeun@github.com>
2023-03-23 18:36:01 +0900

    2493 push

---
+++ week2_스택/2493_trash.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int height[500001];
+    memset(height, 0, 500001);
+    int i;
+    i = 1;
+    while(i <= n)
+    {
+        int tower_height;
+        cin >> tower_height;
+        height[i++] = tower_height;
+    }
+    int j;
+    i = 1;
+    while(i <= n)
+    {
+        // j = i부터 height[j]의 좌측이 height[j]보다 크지 않으면 그 다음 좌측으로. 
+        // j = 1까지 탐색
+        // height[i]보다 큰 순간 i출력하고 그 다음 i확인
+        j = i - 1;
+        while(j >= 1)
+        {
+            if(height[j] > height[i])
+            {
+                cout << j << ' ';
+                break;
+            }
+            j--;
+        }
+        if(j == 0)
+            cout << '0' << ' ';
+        i++;
+    }
+}
