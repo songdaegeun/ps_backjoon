8152252aa59a6dfcab67a32a2e128ef47e2a68ca
sdg <songdaegeun@github.com>
2023-03-24 02:17:54 +0900

    break 3015

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main()
@@ -6,5 +7,30 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-
+    int n, el;
+    long long sum;
+    cin >> n;
+    stack<int> s;
+    sum = 0;
+    while(n--)
+    {
+        cout << '[' << s.size() << ']';
+        sum += s.size();
+        cin >> el;
+        while(s.size() && s.top() < el)
+        {
+            s.pop();
+        }
+        s.push(el);
+    }
+    cout << sum;
 }
+
+// 7
+// 2
+// 4
+// 3
+// 2
+// 1
+// 5
+// 1
