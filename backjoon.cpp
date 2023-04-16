0ba7a44e32677f0bbe8274c52e6da66f7d663245
sdg <songdaegeun@github.com>
2023-04-16 20:16:06 +0900

    6593 tmp

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,121 +1,92 @@
 #include <iostream>
 #include <queue>
 #include <cstring>
-#include <vector>
-#include <algorithm>
+#include <tuple>
 using namespace std;
 
 int n;
-int map[102][102];
-int visited[102][102];
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int l, r, c;
+char map[31][31][31];
+int dist[31][31][31];
+int dx[6] = {1, 0, -1, 0, 0, 0};
+int dy[6] = {0, 1, 0, -1, 0, 0};
+int dz[6] = {0, 0, 0, 0, 1, -1};
 
-void map_copy(int tmp_map[][102]);
-void disp_map(int tmp_map[][102]);
+void disp_map();
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-	
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	while(1)
 	{
-		for (int j = 0; j < n; j++)
+		tuple<int,int,int> s_pt;
+		tuple<int,int,int> e_pt;
+		memset(dist, -1, sizeof(int) * 31 * 31 * 31);
+		cin >> l >> r >> c;
+		cin.ignore();
+		if (!l && !r && !c) break;
+		for(int k = 0; k < l; k++)
 		{
-			cin >> map[i][j];
-		}
-	}
-	int min, max;
-	min = 100;
-	max = 0;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if(min > map[i][j])
-				min = map[i][j];
-			if(max < map[i][j])
-				max = map[i][j];
-		}
-	}
-	int max_cnt = 0;
-	for(int height = min - 1; height <= max; height++)
-	{
-		int tmp_map[102][102];
-		map_copy(tmp_map);
-		memset(visited, 0, sizeof(int) * 102 * 102);
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
+			string str;
+			for(int i = 0; i < r; i++)
 			{
-				if (tmp_map[i][j] <= height)
-					tmp_map[i][j] = 0;
-			}
-		}
-		// disp_map(tmp_map);
-		queue<pair<int, int>> q;
-		int cnt = 0;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if(visited[i][j] || tmp_map[i][j] == 0) continue;
-				cnt++;
-				visited[i][j] = 1;
-				q.push({i,j});
-				while(!q.empty())
+				getline(cin, str);
+				for(int j = 0; j < c; j++)
 				{
-					pair<int,int> pos = q.front();
-					q.pop();
-					int x = pos.first;
-					int y = pos.second;
-					for(int k = 0; k < 4; k++)
-					{
-						int nx = x + dx[k];
-						int ny = y + dy[k];
-						if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-						if(visited[nx][ny] || tmp_map[nx][ny] == 0) continue;
-						visited[nx][ny] = 1;
-						q.push({nx,ny});
-					}
+					map[i][j][k] = str[j];
+					if(map[i][j][k] == 'S')
+						s_pt = make_tuple(i, j, k);
+					else if(map[i][j][k] == 'E')
+						e_pt = make_tuple(i, j, k);
 				}
 			}
+			cin.ignore();
 		}
-		// cout << cnt << '\n';
-		if (max_cnt < cnt)
-			max_cnt = cnt;
+		// cout << get<0>(s_pt) << ' ' << get<1>(s_pt) << ' ' << get<2>(s_pt) << '\n';
+		// cout << get<0>(e_pt) << ' ' << get<1>(e_pt) << ' ' << get<2>(e_pt) << '\n';
+		
+		queue<tuple<int,int,int>> q;
+		dist[get<0>(s_pt)][get<1>(s_pt)][get<2>(s_pt)] = 0;
+		q.push(s_pt);
+		while(!q.empty())
+		{
+			tuple<int,int,int> pos = q.front();
+			q.pop();
+			int x, y, z;
+			tie(x, y, z) = pos;
+			for(int i = 0; i < 6; i++)
+			{
+				int nx, ny, nz;
+				nx = x + dx[i];
+				ny = y + dy[i];
+				nz = z + dz[i];
+				if(nx < 0 || nx >= r || ny < 0 || ny >= c || nz < 0 || nz >= l) continue;
+				if(dist[nx][ny][nz] != -1 || map[nx][ny][nz] == '#') continue;
+				dist[nx][ny][nz] = dist[x][y][z] + 1;
+				q.push(make_tuple(nx, ny, nz));
+			}
+		}
+		if(dist[get<0>(e_pt)][get<1>(e_pt)][get<2>(e_pt)] == -1)
+			cout << "Trapped!\n";
+		else
+			cout << "Escaped in " << dist[get<0>(e_pt)][get<1>(e_pt)][get<2>(e_pt)] <<  "minute(s).\n";
 	}
-	cout << max_cnt;
 }
 
-void map_copy(int tmp_map[][102])
+void disp_map()
 {
-	for (int i = 0; i < n; i++)
+	for(int k = 0; k < l; k++)
 	{
-		for (int j = 0; j < n; j++)
+		for(int i = 0; i < r; i++)
 		{
-			tmp_map[i][j] = map[i][j];
-		}
-	}
-}
-
-void disp_map(int tmp_map[][102])
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << tmp_map[i][j] << ' ';
+			for(int j = 0; j < c; j++)
+			{
+				cout << map[i][j][k] << ' ';
+			}
+			cout << '\n';
 		}
 		cout << '\n';
 	}
 	cout << '\n';
 }
-// 5
-// 6 8 2 6 2
-// 3 2 3 4 6
-// 6 7 3 3 2
-// 7 2 5 3 6
-// 8 9 5 2 7
