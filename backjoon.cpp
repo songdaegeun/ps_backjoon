3d696ad169d803a6b2ec233df036b6531a8b8b2b
sdg <songdaegeun@github.com>
2023-04-22 05:02:43 +0900

    1780 set

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
 using namespace std;
 
+int map[2188][2188];
+int n;
 
+void disp_map()
+{
+	cout << '\n';
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << map[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 	
-	
+	cin >> n;
+	cin.ignore();
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+		
 }
 
+// 9
+// 0 0 0 1 1 1 -1 -1 -1
+// 0 0 0 1 1 1 -1 -1 -1
+// 0 0 0 1 1 1 -1 -1 -1
+// 1 1 1 0 0 0 0 0 0
+// 1 1 1 0 0 0 0 0 0
+// 1 1 1 0 0 0 0 0 0
+// 0 1 -1 0 1 -1 0 1 -1
+// 0 -1 1 0 1 -1 0 1 -1
+// 0 1 -1 1 0 -1 0 1 -1
