f7660c90d6b4a8a8a5def8fb0b17fb9af660093a
sdg <songdaegeun@github.com>
2023-04-22 04:54:22 +0900

    17478

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int map_search(int n, int r, int c)
-{
-	if(n == 0)
-		return 0;
-	int half_size = (1 << (n - 1));
-	if(r < half_size && c < half_size) return (map_search(n - 1, r, c));
-	else if(r < half_size && c >= half_size) return (half_size * half_size + map_search(n - 1, r, c - half_size));
-	else if(r >= half_size && c < half_size) return (2 * half_size * half_size + map_search(n - 1, r - half_size, c));
-	else return (3 * half_size * half_size + map_search(n - 1, r - half_size, c - half_size));
-}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 	
-	int n, r, c;
-	cin >> n >> r >> c;
-	cout << map_search(n, r, c);
+	
 }
 
-// 2 3 1
