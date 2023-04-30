035521f4cc42f6603382d46537377fddfd617e92
Daegeun Song <dasong@c6r5s8.42seoul.kr>
2023-04-30 16:30:52 +0900

    naver boot

--- type2.cpp
+++ type2.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#define NUM 5
+#define NUM 11
 #define MAX_LEN 16
+#define MEMOVER_HALT(size) if( (size) > 128) return 0
 using namespace std;
 
 vector<char> res;
+int n = NUM;
+string arr[101];
+
 int get_len(string &str)
 {
 	int ret[5] = {1,2,4,8,16};;
@@ -23,45 +27,58 @@ int get_len(string &str)
 	return (ret[ret_idx]);
 }
 
-void sol(string *arr)
+int sol(string *arr)
 {
-	
 	// res에 붙히기 직전에 8보다 작다면 8이 되도록 padding 후 ',' 붙히기.
 	int prev_len = 0;
-	
+	int total_len = 0;
 	for (int i = 0; i < NUM; i++)
 	{
-		vector<char> tmp_res;
 		// arr[i]를 보고 개수를 리턴하는 함수.
 		int cur_len = get_len(arr[i]);
 		// 이전 개수보다 현재 개수가 크면 개수가 그만큼 되도록 padding.
-		while(prev_len != 0 && prev_len < cur_len)
+
+		while(prev_len != 0 && prev_len < cur_len )
 		{
-			tmp_res.push_back('.');
+			res.push_back('.');
 			prev_len++;
 			if(prev_len == 8)
 			{
-				tmp_res.push_back(',');
+				res.push_back(',');
+				total_len += prev_len;
+				prev_len = 0;
 				break;
 			}
 		}
+
 		for (int j = 0; j < cur_len; j++)
 		{
-			tmp_res.push_back('#');
-			if(j == 7)
-				tmp_res.push_back(',');
+			res.push_back('#');
+			prev_len++;
+			if(prev_len % 8 == 0) 
+			{
+				res.push_back(',');
+				total_len += prev_len;
+				prev_len = 0;
+			}
 		}
-		for (int j = 0; j < tmp_res.size(); j++)
+		if((prev_len == 0) && (i == NUM - 1))
 		{
-			res.push_back(tmp_res[j]);
+			res.pop_back();
+			prev_len = 8;
 		}
-		if(tmp_res.back() == ',')
-		{
-			prev_len = 0;
-		}
-		else
-			prev_len += tmp_res.size();
+		if(total_len > 128)
+			return (0);
 	}
+	total_len += prev_len;
+	if(total_len > 128)
+		return (0);
+	while (prev_len < 8)
+	{
+		res.push_back('.');
+		prev_len++;
+	}
+	return (1);
 }
 
 int main()
@@ -69,17 +86,24 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 	
-	string arr[101];
-	int n = NUM;
 	for (int i = 0; i < n; i++)
 	{
 		getline(cin, arr[i], ',');
-	}
-	sol(arr);
-	for(auto ch:res)
+	}	
+	if(!sol(arr))
+		cout << "HALT";
+	else
 	{
-		cout << ch ;
+		for(auto ch:res)
+		{
+			cout << ch ;
+		}
 	}
 }
 
 // INT,INT,BOOL,SHORT,LONG,
+// INT,SHORT,FLOAT,INT,BOOL,
+// FLOAT,SHORT,BOOL,BOOL,BOOL,INT,
+// BOOL,LONG,SHORT,LONG,BOOL,LONG,BOOL,LONG,SHORT,LONG,LONG,
+
+// BOOL,LONG,SHORT,LONG,
