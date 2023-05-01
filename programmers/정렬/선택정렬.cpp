1a194cb42f878d9bb18ac9431095383f211d5d8c
songdaegeun <songdaegeun@github.com>
2023-05-01 14:39:45 +0900

    selected sort

---
+++ programmers/정렬/선택정렬.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#define METHOD1
+// #define METHOD2
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[10] = {8,2,85,89,564,4,1,854,41,41};
+    #ifdef METHOD1
+    // 큰 걸 뒤로 보내는 방법
+    for (int i = 9; i > 0; i--)
+    {
+        int maxidx = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            if(arr[maxidx] < arr[j]) maxidx = j;
+        }
+        int tmp = arr[maxidx];
+        arr[maxidx] = arr[i];
+        arr[i] = tmp;
+    }
+    #endif
+    #ifdef METHOD2
+    // 작은 걸 앞으로 보내는 방법
+    for(int i = 0; i < 10; i++)
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    #endif
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+}
--- test.cpp
+++ test.cpp
@@ -1,51 +1,11 @@
-#include <string>
-#include <vector>
+
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-vector<int> v;
-
-int is_sosu(int num)
+int main()
 {
-    if(num < 2)
-        return (0);
-    int i = 2;
-    while(i * i < num)
-    {
-        if(num % i == 0)
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
-
-int solution(string numbers) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
     
-    int answer = 0;
-    for(auto ch : numbers)
-    {
-        v.push_back(ch - '0');
-    }
-    vector<int> idx_v;
-    for(int i = 0; i < v.size(); i++)
-        idx_v.push_back(i);
-    do
-    {
-        
-        int num = 0;
-        for(int i = 0; i < idx_v.size(); i++)
-        {
-            cout << idx_v[i] << ' ';
-            num += v[idx_v[i]];
-        }
-        cout << '\n';
-        if(is_sosu(num))
-            answer++;
-    }while(next_permutation(idx_v.begin(), idx_v.end()));
-    
-    return answer;
 }
