45e760b8db8f182069159e6bed3bea788bd1db14
songdaegeun <eorms6199@gmail.com>
2023-05-04 22:10:51 +0900

    6603 set

--- test.cpp
+++ test.cpp
@@ -1,52 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int arr[8];
-int visited[8];
-int tmp[8];
-int n, m;
-
-void permu(int depth)
-{
-	if(depth == m)
-	{
-		for (int i = 0; i < m; i++)
-		{
-			cout << arr[tmp[i]] << ' ';
-		}
-		cout << '\n';
-		return ;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		// if(!visited[i])
-		// {
-		// 	visited[i] = 1;
-		tmp[depth] = i;
-		permu(depth + 1);
-		// 	visited[i] = 0;
-		// }
-	}
-	
-}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 	
-    
-    cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-	}
-	sort(arr, arr + n);
-	permu(0);
-
 }
 
-// 3 1
-// 4 5 2
+// 7 1 2 3 4 5 6 7
+// 8 1 2 3 5 8 13 21 34
+// 0
