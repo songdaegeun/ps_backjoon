7a073ef3695092e4341caed57656cc9da999503c
Daegeun Song <eorms6199@gmail.com>
2023-05-08 16:58:41 +0900

    11652 ㄴㄷㅅ

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,36 +1,13 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<char> arr[51];
 int n;
+long long arr[100001];
+long long cnt[100001];
 
-int rule(vector<char> a, vector<char> b) {
-    if(a.size() != b.size()) {
-        return (a.size() < b.size());
-    } else{
-        int sum_a = 0;
-        int sum_b = 0;
-        for(auto ch: a) {
-            if(ch >= '0' && ch <= '9')
-                sum_a += ch - '0';
-        }
-        for(auto ch: b) {
-            if(ch >= '0' && ch <= '9')
-                sum_b += ch - '0';
-        }
-        if(sum_a != sum_b)
-            return (sum_a < sum_b);
-    } 
-    // 사전순비교
-    int i = 0;
-    while(i < a.size() && i < b.size()) {
-        if(a[i] != b[i]) {
-            return (a[i] < b[i]);
-        }
-        i++;
-    }
-    return (0);
+int compare(long long a, long long b) {
+
 }
 
 int main()
@@ -39,27 +16,38 @@ int main()
     cin.tie(0);
     
     cin >> n;
-    cin.ignore();
     for (int i = 0; i < n; i++)
     {
-        string str;
-        getline(cin, str);
-        for (int j = 0; str[j]; j++) {
-            arr[i].push_back(str[j]);
-        }
+        cin >> arr[i];
     }
-    sort(arr, arr + n, rule);
-    for (int i = 0; i < n; i++) {
-        for(auto ch : arr[i]) {
-            cout << ch;
-        }
-        cout << '\n';
+
+    // 가장 많이 가지고 있는 정수를 출력.
+    // 가장 많이 가지고 있는 정수가 같다면, 작은 것을 출력.
+    // arr의 요소가 long long으로 들어오므로 counting sort는 불가.
+    
+    // n개의 요소를 내림차순 정렬한다.
+    sort(arr, arr + n, greater<>());
+    // arr를 순회하면서 해당 요소가 이전 요소와 같을 경우, idx번째의 cnt를 올린다.
+    // idx는 0부터 시작하고, 요소가 달라질때마다 변한다.
+    // idx가 가리키는 arr의 요소는 내림차순이다.
+    // cnt를 내림차순 정렬한다.
+    // idx가 가리키는 arr의 요소는 오름차순이 된다.
+
+    int idx = 0;
+    cnt[idx]++;
+    for (int i = 1; i < n; i++)
+    {
+        if(arr[i - 1] == arr[i])
+            cnt[idx]++;
+        else
+            idx++;
     }
+    
 }
 
 // 5
-// ABCD
-// 145C
-// A
-// A910
-// Z321
+// 1
+// 2
+// 1
+// 2
+// 1
