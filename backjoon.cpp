9e721880438e8cecd274fc0437a10b55f2f7901e
Daegeun Song <eorms6199@gmail.com>
2023-05-08 18:29:02 +0900

    11652 working

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int n;
 long long arr[100001];
 long long cnt[100001];
 
-int compare(long long a, long long b) {
-
-}
 
 int main()
 {
@@ -25,24 +23,39 @@ int main()
     // 가장 많이 가지고 있는 정수가 같다면, 작은 것을 출력.
     // arr의 요소가 long long으로 들어오므로 counting sort는 불가.
     
-    // n개의 요소를 내림차순 정렬한다.
-    sort(arr, arr + n, greater<>());
+    // n개의 요소를 오름차순 정렬한다.
+    sort(arr, arr + n, less<long long>());
     // arr를 순회하면서 해당 요소가 이전 요소와 같을 경우, idx번째의 cnt를 올린다.
     // idx는 0부터 시작하고, 요소가 달라질때마다 변한다.
-    // idx가 가리키는 arr의 요소는 내림차순이다.
-    // cnt를 내림차순 정렬한다.
-    // idx가 가리키는 arr의 요소는 오름차순이 된다.
+    // idx가 가리키는 arr의 요소는 오름차순이다.
+    // cnt를 순회해서 max값을 찾는다.
+    // 다시 cnt를 max값과 같은 요소들을 ans_vec에 넣는다.
+    // ans_vec[0]을 출력한다.
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
     int idx = 0;
     cnt[idx]++;
     for (int i = 1; i < n; i++)
     {
-        if(arr[i - 1] == arr[i])
-            cnt[idx]++;
-        else
+        if(arr[i - 1] != arr[i])
+        {
             idx++;
+        }
+        cnt[idx]++;
     }
-    
+    int max = 0;
+    for (int i = 0; i <= idx; i++) {
+        if(max < cnt[i]) max = cnt[i];
+    }
+    vector<int> ans_v;
+    for (int i = 0; i <= idx; i++) {
+        if(max == cnt[i]) ans_v.push_back(cnt[i]);
+    }
+    cout << ans_v[0];
 }
 
 // 5
