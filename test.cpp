bd317ce66eed98e041b68db23eb1179d2a684589
Daegeun Song <eorms6199@gmail.com>
2023-05-08 14:48:39 +0900

    1431

--- test.cpp
+++ test.cpp
@@ -2,10 +2,25 @@
 
 using namespace std;
 
+bool compare(pair<int,int> a, pair<int,int> b) {
+  return a.first > b.first;
+}
+
+pair<int,int> arr[] = {{1,1}, {2,2}};
+int n = sizeof(arr) / sizeof(pair<int,int>);
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-	
+    sort(arr, arr + n, compare);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].second << ' ';
+    }
+    cout << '\n';
+    
+// 출력 결과: 9 6 5 5 5 4 3 3 2 1 1
+
 }
