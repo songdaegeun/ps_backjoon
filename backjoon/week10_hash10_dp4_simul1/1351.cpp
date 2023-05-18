2254a3a4ed41654c44379934d58039b27e4acc8f
sdg <eorms6199@gmail.com>
2023-05-18 13:57:21 +0900

    1351

---
+++ backjoon/week10_hash10_dp4_simul1/1351.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+long long n;
+int p, q;
+unordered_map<long long,long long> serial;
+
+long long find_serial(long long n) {
+	if (n == 0) return 1;				// base condition
+	if (serial[n]) return serial[n];	// base condition
+	// serial[n]이 존재하지 않는 경우
+	serial[n] = find_serial(n / p) + find_serial(n / q);
+	// memoization	
+	return (serial[n]);
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+	cin >> n >> p >> q;
+
+	cout << find_serial(n) << '\n';
+}
+
+// 7 2 3
