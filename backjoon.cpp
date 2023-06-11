4cef152778e784658ab46d08eac0802c01dc924a
Daegeun Song <eorms6199@gmail.com>
2023-06-11 22:46:04 +0900

    9328 solving

--- backjoon.cpp
+++ backjoon.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int tc, n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-int sol;
 
 void disp_map(char map[101][101])
 {
@@ -23,119 +22,57 @@ void disp_map(char map[101][101])
     }
 }
 
-int has_key(char ch, vector<char> &keys)
-{
-    int diff = 'a' - 'A';
-
-    for (auto el: keys)
-    {
-        if((el - diff) == ch)
-            return (1);
-    }
-    return (0);
-}
-
-int is_gate(char ch)
-{
-    if('A' <= ch && ch <= 'Z')
-        return (1);
-    else 
-        return (0);
-}
-
-int bound_entry(char map[101][101], int i, int j, vector<char> &keys)
-{
-    if(i == 0 || i == n - 1 || j == 0 || j == m - 1)
-    {
-        if(map[i][j] == '*')
-            return (0);
-        if(is_gate(map[i][j]))
-        {
-            if(has_key(map[i][j], keys))
-                return (1);
-            else
-                return (0);
-        }
-        else
-        {
-            // '.' key '$'
-            if(map[i][j] == '$')
-               sol++;
-            else if(map[i][j] == '.')
-                ;
-            else
-                keys.push_back(map[i][j]);
-            return (1);
-        }
-    }
-    return (0);
-}
-
-int adjacent(int i, int j, int visited[101][101])
-{
-    for (int k = 0; k < 4; k++)
-    {
-        int nx = i + dx[k];
-        int ny = j + dy[k];
-        if(visited[nx][ny] == 1)
-            return (1);
-    }
-    return (0);
-}
-
-int bsq(char map[][101], vector<pair<int,int>> &entry, vector<char> &keys)
+int bsq(char map[][103], int *keys)
 {
     queue<pair<int,int>> q;
-    int visited[101][101];
-    memset(visited, 0, sizeof(int) * 101 * 101);
+    queue<pair<int,int>> door;
+    int visited[103][103];
+    memset(visited, 0, sizeof(visited));
+    visited[0][0] = 1;
+    q.push({0, 0});
     int cnt = 0;
-    for (auto el:entry)
+    while(!q.empty())
     {
-        visited[el.first][el.second] = 1;
-        q.push({el.first, el.second});
-    }
-    while(1)
-    {
-        while(!q.empty())
+        int x, y;
+        tie(x, y) = q.front(); q.pop();
+        for (int i = 0; i < 4; i++)
         {
-            int x, y;
-            tie(x, y) = q.front(); q.pop();
-            for (int i = 0; i < 4; i++)
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= n + 2 || ny < 0 || ny >= m + 2) continue;
+            // '*' or gate or '$' or '.' or key
+            if(map[nx][ny] == '*' || visited[nx][ny]) continue;
+            if(map[nx][ny] >= 'A' && map[nx][ny] <= 'Z')
             {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if(map[nx][ny] == '*' || visited[nx][ny]) continue;            
-                if(is_gate(map[nx][ny]) && !has_key(map[nx][ny], keys)) continue;
-                if(!is_gate(map[nx][ny]) && map[nx][ny] != '.')
-                {
-                    if(map[nx][ny] == '$')
-                        cnt++;
-                    else
-                        keys.push_back(map[nx][ny]);
+                if(!keys[map[nx][ny] - 'A']) {
+                    visited[nx][ny] = 1;
+                    door.push({nx, ny});
+                }
+                else {
+                    visited[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
+                    
+            }
+            else if(map[nx][ny] >= 'a' && map[nx][ny] <= 'z')
+            {
+                keys[map[nx][ny] - 'a'] = 1;
+                while(!door.empty()) {
+                    q.push(door.front()); door.pop();
                 }
                 visited[nx][ny] = 1;
                 q.push({nx, ny});
             }
-        }
-        // map에 벽이 아니고 방문하지않은 곳 중에 방문한 곳과 인접하고 key가 맞는 문이 있다면 q에 넣고 다시 탐색한다.
-        // 탐색영역이 이전과 그대로면 break.
-        int flag = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
+            else
             {
-                if (visited[i][j] == 0 && is_gate(map[i][j]) && has_key(map[i][j], keys) && adjacent(i, j, visited))
-                {
-                    flag = 1;
-                    visited[i][j] = 1;
-                    q.push({i, j});
-                }
+                if(map[nx][ny] == '$')
+                    cnt++;
+                visited[nx][ny] = 1;
+                q.push({nx, ny});
             }
         }
-        if(flag == 0)
-            break;
     }
+    
     return (cnt);
 }
 
@@ -148,17 +85,16 @@ int main()
     while(tc--)
     {
         cin >> n >> m;
-        char map[101][101];
-        vector<char> keys;
-        vector<pair<int,int>> entry;
-        for (int i = 0; i < n; i++)
+        char map[103][103];
+        memset(map, 0, sizeof(map));
+        int keys[26];
+        memset(keys, 0, sizeof(keys));
+        for (int i = 1; i <= n; i++)
         {
             string tmp;
             cin >> tmp;
             for (int j = 0; j < m; j++)
-            {
-                map[i][j] = tmp[j];
-            }
+                map[i][j + 1] = tmp[j];
         }
         cin.ignore();
         string tmp;
@@ -166,18 +102,9 @@ int main()
         if (tmp != "0")
         {
             for (auto ch: tmp)
-                keys.push_back(ch);
+                keys[ch - 'a'] = 1;
         }
-        sol = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if(bound_entry(map, i, j, keys))
-                    entry.push_back({i, j});
-            }
-        }
-        cout << sol + bsq(map, entry, keys) << '\n';
+        cout << bsq(map, keys) << '\n';
     }
     // 모든 입구를 queue에 넣는다.
     // 키에 해당하는 문은 통과한다.
--- test.cpp
+++ test.cpp
@@ -10,19 +10,7 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-	int n = 3;
-
-	for (int i = 0; i < n - 1; i++)
-	{
-		chosen_oper[i] = i + '0';
-	}
-	do
-	{
-		for (int i = 0; i < n - 1; i++)
-			cout << chosen_oper[i] << ' ';
-		cout << '\n';
-		
-	} while (next_permutation(chosen_oper, chosen_oper + 2));
+	cout << 'a' - 'A' << '\n';
 }
 // N개의 수와 N-1개의 연산자가 주어졌을 때, 만들 수 있는 식의 결과가 최대인 것과 최소인 것을 구하는 프로그램을 작성하시오.
 
