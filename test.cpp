cf71f760af82f72ecffb041d4aba530b2f2f87a0
Daegeun Song <eorms6199@gmail.com>
2023-06-28 17:43:06 +0900

    ps

--- test.cpp
+++ test.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int *p = (int *)malloc(sizeof(int)); // a1
 
-using namespace std;
-
-int main(int argc, char *argv[]) {
-	
-	while(1) {
-		sleep(1);
-		cout << "running..\n";
+	printf("(%d) memory address of p: %08x\n", getpid() , p); // a2
+	*p = 0; // a3
+	while (1) {
+		*p = *p + 1;
+		printf("(%d) p: %d\n ", getpid() , *p); // a4
 	}
 	return 0;
-}
+ }
