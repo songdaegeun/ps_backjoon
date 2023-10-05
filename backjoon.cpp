21478d01a6a633636dafdf49a2dd0a88a117415d
sdg <eorms6199@gmail.com>
2023-10-05 22:09:49 +0900

    10474

--- backjoon.cpp
+++ backjoon.cpp
@@ -6,5 +6,19 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-
+	int num, den;
+	cin >> num >> den;
+	while(!(num==0&&den==0)) {
+		cout << num / den << ' ' << num % den << " / " << den << '\n'; 
+		cin >> num >> den;
+	}
 }
+
+// 27 12
+// 2460000 98400
+// 3 4000
+// 0 0
+
+// 2 3 / 12
+// 25 0 / 98400
+// 0 3 / 4000
