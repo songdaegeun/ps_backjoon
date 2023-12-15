954887ae315fd62b98cfe5b05f5158415b5de304
sdg <eorms6199@gmail.com>
2023-12-15 19:28:13 +0900

    review sort

--- backjoon.cpp
+++ backjoon.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int k;
+int var[14];
+int mask[14];
+
 int main() {
-	vector<int> v1,v2;
-	v1={1,2,3};
-	v2 = v1;
-	for(auto el:v2) {
-		cout << el << ' ';
+	while(1) {
+		cin >> k;
+		if(k == 0)
+			break;
+		for (int i = 0; i < k; i++)
+		{
+			cin >> var[i];
+		}
+		fill(mask+6,mask+k ,1);
+		do {
+			for (int i = 0; i < k; i++)
+			{
+				if(!mask[i]) {
+					cout << var[i] << ' ';
+				}
+			}
+			cout << '\n';
+		} while(next_permutation(mask, mask+k));
+		cout << '\n';
 	}
-	cout << '\n';
+	
+	
 
 	return (0);
 }
+
+// 7 1 2 3 4 5 6 7
+// 8 1 2 3 5 8 13 21 34
+// 0
